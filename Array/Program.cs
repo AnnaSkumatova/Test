5a40929cde7fbcbd4882e3601992c8c238138575
Anna <anut04ka2010@mail.ru>
2022-08-04 00:15:49 +0800

    Начало положено

---
+++ Array/Program.cs
@@ -0,0 +1,30 @@
+﻿
+string[] CreateArray(string size)
+{
+    string[] newArray = new string[size];
+    Console.WriteLine("Creating array here:");
+    
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Input {i + 1} element: ");
+        newArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine();
+    return newArray;
+}
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    
+    Console.WriteLine();
+}
+Console.Write("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value of elements: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value of elements: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArray(size);
+ShowArray(array);
